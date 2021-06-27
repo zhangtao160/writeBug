dd409ae59d688161567978f2008031a7bbf6027b
zhangtao <936970529@qq.com>
2021-06-27 15:58:59 +0800

    writeBug first commit

---
+++ src/test/java/com/tecexchange/wirtebug/WirtebugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecexchange.wirtebug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WirtebugApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
